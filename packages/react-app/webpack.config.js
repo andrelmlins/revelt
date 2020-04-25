321cdfcd8c677f1d54439eae78d0fa9ff3c211d3
André Lins <andrelucas01@hotmail.com>
2020-04-25 12:20:55 -0300

    react: Optimization in webpack

--- packages/react-app/webpack.config.js
+++ packages/react-app/webpack.config.js
@@ -52,6 +52,30 @@ const config = {
     ]
   },
 
+  optimization: {
+    minimize: true,
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: "async",
+      minSize: 1000,
+      minChunks: 2,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name: true,
+      cacheGroups: {
+        default: {
+          minChunks: 1,
+          priority: -20,
+          reuseExistingChunk: true
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
+    }
+  },
+
   plugins: [
     new MiniCssExtractPlugin({
       filename: "static/css/main.css"
