c59d97dc0808bc1e08bf4992071ed84c36df3a44
André Lins <andrelucas01@hotmail.com>
2020-05-30 11:01:24 -0300

    Create header and removing files

---
+++ packages/vue-app/src/components/Header/index.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <header>
+    <a href="/">
+      <strong>Vue</strong>
+      x React x Svelte
+    </a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+header {
+  color: black;
+  background-color: #3eb37f;
+  position: static;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1100;
+  box-sizing: border-box;
+  flex-shrink: 0;
+  flex-direction: column;
+  min-height: 64px;
+}
+a {
+  font-size: 30px;
+  margin: 0;
+  color: white;
+  font-weight: 600;
+  text-shadow: 1px 1px 2px #333;
+  text-decoration: none;
+}
+a strong {
+  font-weight: 900;
+}
+</style>
