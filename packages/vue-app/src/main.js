9f5ab783cf2423cb901c81e26e9e3c53df5b37c5
André Lins <andrelucas01@hotmail.com>
2020-06-01 09:17:01 -0300

    vue: List pokemons

--- packages/vue-app/src/main.js
+++ packages/vue-app/src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./core/App.vue";
+import { Row, Column } from "vue-grid-responsive";
 import router from "./core/router";
 import vueHeadful from "vue-headful";
 
 Vue.component("vue-headful", vueHeadful);
+Vue.component("row", Row);
+Vue.component("column", Column);
 Vue.config.productionTip = false;
 
 new Vue({
--- packages/vue-app/src/screens/List/index.vue
+++ packages/vue-app/src/screens/List/index.vue
@@ -1,23 +1,39 @@
 <template>
   <div>
-    <CardPokemon :pokemon="pokemon" />
+    <Loader v-if="loading" />
+    <vue-headful v-if="!loading" title="Pokédex" />
+    <row :gutter="12" v-if="!loading">
+      <column
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        :xs="12"
+        :sm="6"
+        :lg="4"
+      >
+        <CardPokemon :pokemon="pokemon" />
+      </column>
+    </row>
   </div>
 </template>
 
 <script>
 import Loader from "../../components/Loader";
 import CardPokemon from "../../containers/CardPokemon";
+import { allPokemons } from "../../services/pokemons";
 
 export default {
   name: "List",
   data() {
     return {
-      pokemon: {
-        id: 1,
-        name: "bulbasaur",
-        url: "https://pokeapi.co/api/v2/pokemon/1/"
-      }
+      pokemons: [],
+      loading: true
     };
+  },
+  async mounted() {
+    const data = await allPokemons(0);
+    this.pokemons = [...this.pokemons, ...data.results];
+
+    setTimeout(() => (this.loading = false), 2000);
   },
   components: {
     Loader,
