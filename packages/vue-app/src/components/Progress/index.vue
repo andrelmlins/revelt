f2246302d7fdff78b3a1c7b3298641b46c0a202e
André Lins <andrelucas01@hotmail.com>
2020-06-01 19:24:37 -0300

    vue: Create progress component

---
+++ packages/vue-app/src/components/Progress/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="root-progress">
+    <label :for="label">{{label}}</label>
+    <progress :id="label" :value="value" max="100" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Progress",
+  props: {
+    label: String,
+    value: String
+  }
+};
+</script>
+
+<style scoped>
+.root-progress {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+progress {
+  width: 100%;
+  -webkit-appearance: none;
+  margin-top: 3px;
+}
+
+progress::-webkit-progress-bar {
+  border-radius: 5px;
+  height: 10px;
+  background-color: #9e9e9e;
+}
+
+progress::-webkit-progress-value {
+  border-radius: 5px;
+  background-color: #386aad;
+}
+
+progress::-moz-progress-value {
+  border-radius: 5px;
+  background-color: #386aad;
+}
+
+progress::-moz-progress-bar {
+  border-radius: 5px;
+  height: 10px;
+  background-color: #9e9e9e;
+}
+</style>
+
